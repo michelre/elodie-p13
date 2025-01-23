7475ddeae9c13f5e4d3a0c49d8fb9bdb1cf8bc6a
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2025-01-23 16:01:39 +0100

    api

--- elodie-p-13/src/api.js
+++ elodie-p-13/src/api.js
@@ -1,40 +1,38 @@
 const signin = (email, password) => {
-    return fetch('http://127.0.0.1:3001/api/v1/user/login', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email, password
-        })
-    })
-}
+  return fetch("http://127.0.0.1:3001/api/v1/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+};
 
 const getUserProfile = (token) => {
-    return fetch('http://127.0.0.1:3001/api/v1/user/profile', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-    })
-}
+  return fetch("http://127.0.0.1:3001/api/v1/user/profile", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 
 const setUserProfile = (firstName, lastName, token) => {
-    return fetch('http://127.0.0.1:3001/api/v1/user/profile', {
-        method: 'PUT',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        body: JSON.stringify({
-            firstName, lastName
-        })
-    })
-}
+  return fetch("http://127.0.0.1:3001/api/v1/user/profile", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      firstName,
+      lastName,
+    }),
+  });
+};
 
-export {
-    signin,
-    getUserProfile,
-    setUserProfile,
-}
+export { signin, getUserProfile, setUserProfile };
